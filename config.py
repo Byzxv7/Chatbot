cc7ba6b95c5c4c7920cdc763d6069e4958054c2b
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2021-10-23 05:38:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-OWNER = os.environ.get("OWNER", "kenkanasw")
+TOKEN = os.environ.get("TOKEN", "1960247583:AAEls_RRZr-Lu54aPdH3JWkN--9t-V2o-bM")
+ADMIN = int(os.environ.get("ADMIN", "1430773637"))
+OWNER = os.environ.get("OWNER", "Bydznesiaaa")
 GROUP = os.environ.get("GROUP", "musikkugroup")
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
-PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
+CHANNEL = os.environ.get("CHANNEL", "rpchatbotxx")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "roleplay chat bot")
