3aa127ec12be19fb02a8adee9d4106f3f9512d1c
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2022-01-29 21:23:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ TOKEN = os.environ.get("TOKEN", "1960247583:AAEls_RRZr-Lu54aPdH3JWkN--9t-V2o-bM"
 ADMIN = int(os.environ.get("ADMIN", "2134190388"))
 OWNER = os.environ.get("OWNER", "Xdcv7")
 GROUP = os.environ.get("GROUP", "bandarpinguinxxx")
-CHANNEL = os.environ.get("CHANNEL", "bandarpinguinxx")
+CHANNEL = os.environ.get("CHANNEL", "rpchatbotxx")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "Anonymous Chat 18+")
