4725defde016fc4d772734be2f638905d4f790e0
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2022-03-15 20:10:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5202965388:AAE5fHXK7o-euB0X5vsKqQ6541ndjHCzbHY")
-ADMIN = int(os.environ.get("ADMIN", "1430773637"))
+TOKEN = os.environ.get("TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
 OWNER = os.environ.get("OWNER", "")
-GROUP = os.environ.get("GROUP", "vexana0x")
-CHANNEL = os.environ.get("CHANNEL", "vexana0x")
-PROJECT_NAME = os.environ.get("PROJECT_NAME", "Random chat RP bot")
+GROUP = os.environ.get("GROUP", "")
+CHANNEL = os.environ.get("CHANNEL", "")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "")
