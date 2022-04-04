256b79020d0ebe781dbfece29ccd07ff65ddd73c
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2022-04-04 18:45:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-OWNER = os.environ.get("OWNER", "")
-GROUP = os.environ.get("GROUP", "")
-CHANNEL = os.environ.get("CHANNEL", "")
-PROJECT_NAME = os.environ.get("PROJECT_NAME", "")
+TOKEN = os.environ.get("TOKEN", "5202965388:AAE5fHXK7o-euB0X5vsKqQ6541ndjHCzbHY")
+ADMIN = int(os.environ.get("ADMIN", "1430773637"))
+OWNER = os.environ.get("OWNER", "Bydznesiaaa")
+GROUP = os.environ.get("GROUP", "vexana0x")
+CHANNEL = os.environ.get("CHANNEL", "vexana0x")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "roleplayer bot")
