46b9442a03a29c88e14cea66d387d189ddc7e0b6
Vexana <92144730+Byzxv7@users.noreply.github.com>
2022-11-27 16:40:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5202965388:AAE5fHXK7o-euB0X5vsKqQ6541ndjHCzbHY")
-ADMIN = int(os.environ.get("ADMIN", "1430773637"))
-OWNER = os.environ.get("OWNER", "Bydznesiaaa")
-GROUP = os.environ.get("GROUP", "vexana0x")
-CHANNEL = os.environ.get("CHANNEL", "vexana0x")
-PROJECT_NAME = os.environ.get("PROJECT_NAME", "roleplayer bot")
+TOKEN = os.environ.get("TOKEN", "5852785414:AAFr5R621IDOSOwUU_aLOBR1kdnNsGs2wfk")
+ADMIN = int(os.environ.get("ADMIN", "5148590664"))
+OWNER = os.environ.get("OWNER", "Butzyc")
+GROUP = os.environ.get("GROUP", "chatrpx")
+CHANNEL = os.environ.get("CHANNEL", "chatrpx")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "roleplay bot")
